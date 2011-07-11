7a3332b7e7ee70f815c89e7725854fac5c058c9d
Shaun Crampton <Shaun.Crampton@metaswitch.com>
2011-07-10 18:46:36 -0700

    Rework for wooden chassis, still some vibration left

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "WProgram.h"
 #include "cppboilerplate.h"
 #include <avr/interrupt.h>
+#include <util/atomic.h>
 
 int pwm_a = 3;  //PWM control for motor outputs 1 and 2 is on digital pin 3
 int pwm_b = 11;  //PWM control for motor outputs 3 and 4 is on digital pin 11
@@ -12,8 +13,10 @@ int x_pin = A0;
 int y_pin = A1;
 int gyro_pin = A2;
 
-const float GYRO_OFFSET = 3.45;
-const float Y_OFFSET = -7;    // More negative tilts forwards
+const float GYRO_OFFSET = 4.36;
+const float Y_OFFSET = 9;    // More negative tilts forwards
+
+//#define CALIBRATION
 
 #define RESET_TIMER1() TCNT1 = 0xC000
 
@@ -34,7 +37,18 @@ static float filtery(float in)
   yv[2] =   (xv[0] + xv[2]) + 2 * xv[1]
                + ( -0.9131487721 * yv[0]) + (  1.9092019151 * yv[1]);
   return yv[2];
- }
+}
+
+static float filtergyro(float in)
+{
+  static float xv[NZEROS+1], yv[NPOLES+1];
+  xv[0] = xv[1]; xv[1] = xv[2];
+  xv[2] = in / 1.565078650e+00;
+  yv[0] = yv[1]; yv[1] = yv[2];
+  yv[2] =   (xv[0] + xv[2]) + 2 * xv[1]
+               + ( -0.4128015981 * yv[0]) + ( -1.1429805025 * yv[1]);
+  return yv[2];
+}
 
 void myprintf(const char *fmt, ... ){
   char tmp[128]; // resulting string limited to 128 chars
@@ -45,28 +59,34 @@ void myprintf(const char *fmt, ... ){
   Serial.print(tmp);
 }
 
+static int gyro_reading = 0;
+static int x_reading = 0;
+static int y_reading = 0;
+
 ISR(TIMER1_OVF_vect)
 {
   float y;
   float x;
   float d_tilt;
-  long int speed = 0;
-  long int last_speed = 0;
+  float speed = 0;
+  static float last_speed = 0;
   long int motor_a_speed = 0;
   long int motor_b_speed = 0;
   static float tilt = 0;
   static float y_filt = 0;
-  static float y_filt_int = 0;
   static float tilt_int = 0;
   static boolean reset_complete = false;
 
   // Read the gyro rate.
   RESET_TIMER1();
-  d_tilt = 512 - analogRead(gyro_pin) + GYRO_OFFSET;
+  gyro_reading = analogRead(gyro_pin);
+  d_tilt = (512 - gyro_reading + GYRO_OFFSET);
 
   // Read the accelerometer
-  y = analogRead(x_pin) - 512 + Y_OFFSET;
-  x = analogRead(y_pin) - 512;
+  x_reading = analogRead(x_pin);
+  y_reading = analogRead(y_pin);
+  y = x_reading - 512 + Y_OFFSET;
+  x = y_reading - 512;
 
   y_filt = filtery(y);
 
@@ -82,6 +102,7 @@ ISR(TIMER1_OVF_vect)
     if (-5 < y && y < 5)
     {
       tilt = y;
+      tilt_int = 0;
       reset_complete = true;
     }
   }
@@ -90,22 +111,42 @@ ISR(TIMER1_OVF_vect)
     // Normal operation.  Integrate gyro to get tilt.  Add in the filtered
     // accelerometer value which approximates the absolute tilt, this gives
     // us an integral term to correct for drift in the gyro.
-    tilt += d_tilt;
+    tilt += d_tilt + y_filt * 0.3;
     tilt_int += tilt;
-    y_filt_int += y_filt;
-    speed = tilt * 0.003 +
-            y_filt_int * 0.003 +
-            d_tilt * 0.20;
+
+#define D_TILT_FACT 3
+#define TILT_FACT 0.1
+#define TILT_INT_FACT 0.0001
+#define MAX_TILT_INT (255.0 / TILT_INT_FACT)
+
+    if (tilt_int > MAX_TILT_INT)
+    {
+      tilt_int = MAX_TILT_INT;
+    }
+    if (tilt_int < -MAX_TILT_INT)
+    {
+      tilt_int = -MAX_TILT_INT;
+    }
+#ifndef CALIBRATION
+    speed = tilt * TILT_FACT +
+            tilt_int * TILT_INT_FACT +
+            d_tilt * D_TILT_FACT;
+    speed = filtergyro(speed);
+#endif
     last_speed = speed;
   }
 
   digitalWrite(dir_a, speed < 0 ? LOW : HIGH);  //Set motor direction, 1 low, 2 high
   digitalWrite(dir_b, speed < 0 ? LOW : HIGH);  //Set motor direction, 3 high, 4 low
 
-  speed = 16 * sqrt((float)abs(speed));
+  speed = 8 * sqrt(abs(speed));
+  if (speed > 0xff)
+  {
+    speed = 0xff;
+  }
 
-  motor_a_speed = speed * MOTOR_A_FACTOR;
-  motor_b_speed = speed * MOTOR_B_FACTOR;
+  motor_a_speed = (long int)(speed * MOTOR_A_FACTOR);
+  motor_b_speed = (long int)(speed * MOTOR_B_FACTOR);
 
   if (motor_a_speed > 0xff)
   {
@@ -137,14 +178,18 @@ void setup()
   TCCR0B = (TCCR0B & 0xf8) | _BV(CS02);
   TCCR2B = (TCCR0B & 0xf8) | _BV(CS02);
 
-  // Timer1 configuration
+  // Timer1 used for accurate sampling time.
   TCCR1B = _BV(CS10);   // Enable Timer 1; no prescaler
   RESET_TIMER1();
   TIMSK1 = _BV(TOIE1); // Enable Timer 1 overflow interrupt.
   sei();
 }
-
 void loop()
 {
-  // Nothing to do... the ISR does it all
+#ifdef CALIBRATION
+  ATOMIC_BLOCK(ATOMIC_FORCEON)
+  {
+    myprintf("Gyro: %d, X: %d, Y: %d\r", gyro_reading, x_reading, y_reading);
+  }
+#endif
 }
